4cb6e5cf5b324a24c80faa6439c69dde27ce50e3
Rene29Alexander <rene29alexander@gmail.com>
2021-09-28 18:24:38 -0600

    Agregar archivos de proyecto.

---
+++ Herencia/Autobus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Autobus:Vehiculo.Vehiculo
+
+    {
+        
+        public int numeroPlazas { get; set; }
+
+
+        
+        public virtual string ruta()
+        {
+            return "La ruta del autobus es de San Miguel a San Salvador";
+        }
+
+        //metodo sobreescrito
+        public override string getmodelo()
+        {
+            
+            return "ferrari La ferrari";
+        }
+    }
+}
---
+++ Herencia/Taxi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Taxi: Vehiculo.Vehiculo
+    {
+        public int numeroLicencia { get; set; }
+
+        public string TaxisRegion()
+        {
+            return "La region del taxi es san miguel";
+        }
+
+        public override string getMatricula()
+        {
+            
+            return "tiene licencia de taxista";
+        }
+
+
+
+
+    }
+}
